2d376c47b02b82095c613e4f00764ac35620a14f
Tomas Sanchez Negrette <tomas.sancheznegrette@emeal.nttdata.com>
2024-06-26 15:22:02 -0300

    Add rotation colision detection

--- tetris/src/main.c
+++ tetris/src/main.c
@@ -32,10 +32,7 @@ player_t player;
 
 pthread_mutex_t mutex_positions;
 
-int get_block_row_col(int block_index, int *row, int *col) {
-  if (player.bounding_box[block_index] == 0)
-    return 0;
-
+void get_block_row_col(int block_index, int *row, int *col) {
   int bb_col = block_index % 4;
   int bb_row = block_index / 4;
   int bb_center_col = BOUNDING_BOX_CENTER % 4;
@@ -46,7 +43,6 @@ int get_block_row_col(int block_index, int *row, int *col) {
   int block_row = player.position[0] - offset_row;
   *row = block_row;
   *col = block_col;
-  return 1;
 }
 
 void get_piece(int *r, rotation_t rotation, int buffer[16]) {
@@ -329,12 +325,12 @@ void reset_game() {
 
 void bb_to_board() {
   for (int i = 0; i < 16; i++) {
+    if (player.bounding_box[i] == 0)
+      continue;
     int block_col = 0;
     int block_row = 0;
-    int res = get_block_row_col(i, &block_row, &block_col);
-    if (res == 1) {
-      board[block_row][block_col] = 2;
-    }
+    get_block_row_col(i, &block_row, &block_col);
+    board[block_row][block_col] = 2;
   }
 }
 
@@ -350,11 +346,12 @@ void set_player_piece() {
 
 void reset_bounding_box() {
   for (int i = 0; i < 16; i++) {
+    if (player.bounding_box[i] == 0)
+      continue;
     int block_col = 0;
     int block_row = 0;
-    int res = get_block_row_col(i, &block_row, &block_col);
-    if (res == 1)
-      board[block_row][block_col] = 0;
+    get_block_row_col(i, &block_row, &block_col);
+    board[block_row][block_col] = 0;
     player.bounding_box[i] = 0;
   }
 }
@@ -368,24 +365,24 @@ void player_new_piece() {
 }
 
 void player_rotate(move_t m) {
-  reset_bounding_box();
+  rotation_t next_rotation;
   switch (m) {
   case RIGHT: {
     switch (player.rotation) {
     case NORTH: {
-      player.rotation = EAST;
+      next_rotation = EAST;
       break;
     }
     case EAST: {
-      player.rotation = SOUTH;
+      next_rotation = SOUTH;
       break;
     }
     case SOUTH: {
-      player.rotation = WEST;
+      next_rotation = WEST;
       break;
     }
     case WEST: {
-      player.rotation = NORTH;
+      next_rotation = NORTH;
       break;
     }
     }
@@ -394,27 +391,42 @@ void player_rotate(move_t m) {
   case LEFT: {
     switch (player.rotation) {
     case NORTH: {
-      player.rotation = WEST;
+      next_rotation = WEST;
       break;
     }
     case EAST: {
-      player.rotation = NORTH;
+      next_rotation = NORTH;
       break;
     }
     case SOUTH: {
-      player.rotation = EAST;
+      next_rotation = EAST;
       break;
     }
     case WEST: {
-      player.rotation = SOUTH;
+      next_rotation = SOUTH;
       break;
     }
     }
     break;
   }
   default:
-    break;
+    return;
   }
+  int buffer[16];
+  get_piece(&player.piece, next_rotation, buffer);
+
+  for (int i = 0; i < 16; i++) {
+    if (buffer[i] == 0)
+      continue;
+    int row = 0;
+    int col = 0;
+    get_block_row_col(i, &row, &col);
+    if (board[row][col] == 1 || col >= COLUMNS || col < 0 || row>=ROWS || row<0)
+      return;
+  }
+
+  player.rotation = next_rotation;
+  reset_bounding_box();
   get_piece(&player.piece, player.rotation, player.bounding_box);
   pthread_mutex_unlock(&mutex_positions);
 }
@@ -425,33 +437,32 @@ int player_move(move_t m) {
   case LEFT: {
     int min_col = COLUMNS;
     for (int i = 0; i < 16; i++) {
+      if (player.bounding_box[i] == 0)
+        continue;
       int block_col = 0;
       int block_row = 0;
-      int res = get_block_row_col(i, &block_row, &block_col);
-      if (res == 0)
-        continue;
-
+      get_block_row_col(i, &block_row, &block_col);
       if (block_col < min_col)
         min_col = block_col;
     }
     if (min_col > 0) {
       for (int i = 0; i < 16; i++) {
+        if (player.bounding_box[i] == 0)
+          continue;
         int block_col = 0;
         int block_row = 0;
-        int res = get_block_row_col(i, &block_row, &block_col);
-        if (res == 0)
-          continue;
+        get_block_row_col(i, &block_row, &block_col);
         if (board[block_row][block_col - 1] == 1) {
           pthread_mutex_unlock(&mutex_positions);
           return 1;
         }
       }
       for (int i = 0; i < 16; i++) {
+        if (player.bounding_box[i] == 0)
+          continue;
         int prev_row = 0;
         int prev_col = 0;
-        int res = get_block_row_col(i, &prev_row, &prev_col);
-        if (res == 0)
-          continue;
+         get_block_row_col(i, &prev_row, &prev_col);
         board[prev_row][prev_col] = 0;
       }
       player.position[1]--;
@@ -464,21 +475,21 @@ int player_move(move_t m) {
   case RIGHT: {
     int max_col = 0;
     for (int i = 0; i < 16; i++) {
+      if (player.bounding_box[i] == 0)
+        continue;
       int block_col = 0;
       int block_row = 0;
-      int res = get_block_row_col(i, &block_row, &block_col);
-      if (res == 0)
-        continue;
+      get_block_row_col(i, &block_row, &block_col);
       if (block_col > max_col)
         max_col = block_col;
     }
     if (max_col < COLUMNS - 1) {
       for (int i = 0; i < 16; i++) {
+        if (player.bounding_box[i] == 0)
+          continue;
         int block_col = 0;
         int block_row = 0;
-        int res = get_block_row_col(i, &block_row, &block_col);
-        if (res == 0)
-          continue;
+        get_block_row_col(i, &block_row, &block_col);
 
         if (board[block_row][block_col + 1] == 1) {
           pthread_mutex_unlock(&mutex_positions);
@@ -486,11 +497,11 @@ int player_move(move_t m) {
         }
       }
       for (int i = 0; i < 16; i++) {
+        if (player.bounding_box[i] == 0)
+          continue;
         int block_col = 0;
         int block_row = 0;
-        int res = get_block_row_col(i, &block_row, &block_col);
-        if (res == 0)
-          continue;
+        get_block_row_col(i, &block_row, &block_col);
         board[block_row][block_col] = 0;
       }
       player.position[1]++;
@@ -503,33 +514,32 @@ int player_move(move_t m) {
   case DOWN: {
     int max_row = 0;
     for (int i = 0; i < 16; i++) {
+      if (player.bounding_box[i] == 0)
+        continue;
       int block_col = 0;
       int block_row = 0;
-      int res = get_block_row_col(i, &block_row, &block_col);
-      if (res == 0)
-        continue;
-
+      get_block_row_col(i, &block_row, &block_col);
       if (block_row > max_row)
         max_row = block_row;
     }
     if (max_row < ROWS - 1) {
       for (int i = 0; i < 16; i++) {
+        if (player.bounding_box[i] == 0)
+          continue;
         int block_col = 0;
         int block_row = 0;
-        int res = get_block_row_col(i, &block_row, &block_col);
-        if (res == 0)
-          continue;
+        get_block_row_col(i, &block_row, &block_col);
         if (board[block_row + 1][block_col] == 1) {
           pthread_mutex_unlock(&mutex_positions);
           return 1;
         }
       }
       for (int i = 0; i < 16; i++) {
+        if (player.bounding_box[i] == 0)
+          continue;
         int prev_col = 0;
         int prev_row = 0;
-        int res = get_block_row_col(i, &prev_row, &prev_col);
-        if (res == 0)
-          continue;
+        get_block_row_col(i, &prev_row, &prev_col);
         board[prev_row][prev_col] = 0;
       }
       player.position[0]++;
@@ -634,9 +644,9 @@ int main(int argc, char *argv[]) {
         int last_line_index = 0;
         int cleared_lines = check_line(&last_line_index);
         move_board_down(last_line_index, cleared_lines);
-        check_lost();
         player.piece = player.next_piece;
         player_new_piece();
+        check_lost();
         bb_to_board();
       }
       print_screen();
